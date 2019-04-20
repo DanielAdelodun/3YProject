894cc9f1379007ba945db4c7a10be3304fed11ce
Daniel Ajibola Adelodun <daniel_adelodun@hotmail.co.uk>
2019-04-20 06:58:51 +0100

    Slightly improved the Area Appoximation figures

--- Code/Area2.py
+++ Code/Area2.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np 
-# from scipy.interpolate import interp1d
 
+xmax = 1.5
+xmin = 0
+totalwidth = xmax - xmin
 colour1 = 'red'
 colour2 = 'purple'
 colour3 = 'blue'
+f = np.sin
 
-x = np.linspace(0, 1.5, num=21, endpoint=True)
-y = np.sin(x)
+x = np.linspace(xmin, xmax, num=21, endpoint=True)
+y = f(x)
 
 # sin graph
-xs = np.linspace(0, 1.5, 100)
-ys = np.sin(xs)
+xs = np.linspace(xmin, xmax, 100)
+ys = f(xs)
 
-# Set up axis
-fig, axes = plt.subplots(nrows=3, dpi=300, figsize=(3, 4))
+# Set up axes
+fig, axes = plt.subplots(nrows=3, dpi=300, figsize=(2.5, 4))
+fig.subplots_adjust(hspace = 0.075)
 ax1, ax2, ax3 = axes
 for ax in axes:
     ax.plot(xs, ys, 'g-') 
@@ -26,12 +30,20 @@ for ax in axes:
                    labelbottom=False, 
                    labelright=False,
                    labeltop=False)
-    ax.set_xlim(0, 1.5)
-   
-ax1.bar(x[::4], np.sin(x[::4]), align='edge', width=-(1.5/5), fc=colour3, ec=colour3, alpha=0.3)
-ax2.bar(x[::2], np.sin(x[::2]), align='edge', width=-(1.5/10), fc=colour2, ec=colour2, alpha=0.3)
-ax3.bar(x[:], np.sin(x[:]), align='edge', width=-(1.5/20), fc=colour1, ec=colour1, alpha=0.3)
+    ax.set_xlim(xmin, xmax)
 
-fig.tight_layout()
+x4 = x[::4]
+x2 = x[::2]
+x1 = x[::1]
+ax1.bar(x4, f(x4), 
+    align='edge', width=-(totalwidth/5), 
+    fc=colour1, ec=colour1, alpha=0.3)
+ax2.bar(x2, f(x2),
+    align='edge', width=-(totalwidth/10),
+    fc=colour2, ec=colour2, alpha=0.3)
+ax3.bar(x1, f(x1), 
+    align='edge', width=-(totalwidth/20),
+    fc=colour3, ec=colour3, alpha=0.3)
+
 fig.savefig('Area2.png')
-plt.show()
+#plt.show()
--- Code/Area4.py
+++ Code/Area4.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
-xmax = 1.5
+xmax = 2
 xmin = 0
 totalwidth = xmax - xmin
 colour1 = 'red'
@@ -9,19 +9,20 @@ colour2 = 'purple'
 colour3 = 'blue'
 f = np.sin
 
+# 20 points equally spaced points (plus zero) in the domain
 x = np.linspace(xmin, xmax, num=21, endpoint=True)
 y = f(x)
 
-# sin graph
+# To draw graph
 xs = np.linspace(xmin, xmax, 100)
 ys = f(xs)
 
 # Set up axes
-fig, axes = plt.subplots(nrows=3, dpi=300, figsize=(2.5, 4))
+fig, axes = plt.subplots(nrows=3, dpi=300, figsize=(2.5, 3.5))
 fig.subplots_adjust(hspace = 0.075)
 ax1, ax2, ax3 = axes
 for ax in axes:
-    ax.plot(xs, ys, 'g-') 
+    ax.plot(xs, ys, 'g-', linewidth=0.5) 
     ax.tick_params(top=False, 
                    bottom=False, 
                    left=False, 
@@ -30,20 +31,32 @@ for ax in axes:
                    labelbottom=False, 
                    labelright=False,
                    labeltop=False)
+    for axis in ['top','bottom','left','right']:
+      ax.spines[axis].set_linewidth(0.4)
+
     ax.set_xlim(xmin, xmax)
 
-x4 = x[:-1:4]
-x2 = x[:-1:2]
-x1 = x[:-1:1]
-ax1.bar(x4, f(x4), 
+# Skinny --> Wide
+x1, x2, x4 = [x[:-1:2**n] for n in range(3)]
+bar1, bar2, bar4 = [(x, []) for x in [x1, x2, x4]]
+for interval_steps, bar in zip([x1, x2, x4], [bar1, bar2, bar4]):
+    for i in range(len(interval_steps)):
+        xright = xmax if i == len(interval_steps) - 1 else interval_steps[i+1]
+        xleft = interval_steps[i]
+        interval = np.linspace(xleft, xright, 1000)
+        interval_mapped = [f(x) for x in interval]
+        interval_max = max(interval_mapped)
+        bar[1].append(interval_max)
+
+ax1.bar(*bar4, 
     align='edge', width=(totalwidth/5), 
     fc=colour1, ec=colour1, alpha=0.3)
-ax2.bar(x2, f(x2),
+ax2.bar(*bar2,
     align='edge', width=(totalwidth/10),
     fc=colour2, ec=colour2, alpha=0.3)
-ax3.bar(x1, f(x1), 
+ax3.bar(*bar1, 
     align='edge', width=(totalwidth/20),
     fc=colour3, ec=colour3, alpha=0.3)
 
-fig.savefig('Area1.png')
+fig.savefig('Area4.png')
 #plt.show()
